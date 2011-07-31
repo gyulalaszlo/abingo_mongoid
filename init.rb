e4d14d1f7fc8763998755a70a4ff30a331ea16a1
Gyula Laszlo <gyula.laszlo.gm@gmail.com>
2011-07-31 22:46:03 +0200

    refactored views to haml

--- init.rb
+++ init.rb
@@ -1,7 +1,4 @@
 require File.dirname(__FILE__) + '/lib/abingo'
-if Rails::VERSION::MAJOR >= 3
-  require File.dirname(__FILE__) + '/generators/abingo_migration/abingo_migration_generator.rb'
-end
 
 ActionController::Base.send :include, AbingoSugar
 
